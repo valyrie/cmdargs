74612f76e9aba645970b311a9248ddbe6b49d832
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2022-02-04 11:37:37 +0000

    complete rewrite of cmdargs, code broken in submodules, help/usage improved

--- cmdargs.ml
+++ cmdargs.ml
@@ -1,14 +1,11 @@
 (* main module *)
 
-(* exceptions *)
 exception Unknown_switch of string
 exception Print of string
 exception Print_help
 exception Print_usage
 exception Not_enough_arguments
 exception Too_many_arguments
-
-(* type submodules *)
 module Store = struct
     type t =
         One of string ref
@@ -35,116 +32,105 @@ module Effect = struct
         | Set_bool of bool ref
         | Inc_int of int ref
 end
-
-(* command description *)
-type cmdopt = {
-    effect: Effect.t;
-    switches: string list;
-    help: string option
-}
-type cmdarg = {
-    metavar: string;
-    store: Store.t;
-    help: string option
-}
-type cmd = {
-    name: string;
-    opts: cmdopt list;
-    args: cmdarg list;
-    synopsis: string option;
-    epilogue: string option
-}
-
-(* internal functions *)
-let first_chars str len =
-    String.sub str 0 len
-let string_after str pos =
-    String.sub str pos @@ (String.length str) - pos
-let switch_prefix = "-"
-let is_switch arg =
-    first_chars arg (String.length switch_prefix) = switch_prefix
-let strip_switch sw =
-    if is_switch sw then
-        let sw = string_after sw 1 in
-        if is_switch sw then
-            string_after sw 1
-        else
-            sw
-    else
-        raise @@ Invalid_argument ""
-let prefix_switch sw =
-    String.concat "" [switch_prefix; sw]
-let bracketize s =
-    String.concat "" ["["; s; "]"]
-let lookup_cmdopt_opt sw opts =
-    List.find_opt
-        (fun opt ->
-            List.exists
-                (fun switch ->
-                    switch = strip_switch sw) opt.switches) opts
-let lookup_cmdopt sw opts =
-    match lookup_cmdopt_opt sw opts with
-        Some opt ->
-            opt
-        | None ->
-            raise @@ Unknown_switch sw
-let known_switch sw opts =
-    match lookup_cmdopt_opt sw opts with
-        None -> false
-        | _ -> true
-(* public functions *)
-let usage cmd =
-    Printf.sprintf
-        "usage: %s [OPTION ...] [ARG ...]"
-        cmd.name
-let help cmd =
-    let switches_width =
-        List.fold_left
-            (fun w opt ->
-                let s =
-                    String.concat ", "
-                    @@ List.map prefix_switch
-                        opt.switches in
-                max (String.length s) w)
-            0
-            cmd.opts in
-    let arguments_width =
-        List.fold_left
-            (fun w arg ->
-                max (String.length arg.metavar) w)
-            0
-            cmd.args in
-    String.concat "\n\n"
-        @@ List.filter
-            (fun s -> String.trim s != "")
+module Switch = struct
+    let first_chars str len =
+        String.sub str 0 len
+    let string_after str pos =
+        String.sub str pos @@ (String.length str) - pos
+    let rec is_switch sw prefixes =
+        match prefixes with
+            [] -> false
+            | prefix :: prefixes ->
+                if first_chars sw @@ String.length prefix = prefix then
+                    true
+                else
+                    is_switch sw prefixes
+    let rec strip_prefix sw prefixes =
+        match prefixes with
+            [] -> raise @@ Invalid_argument ""
+            | prefix :: prefixes ->
+                if first_chars sw @@ String.length prefix = prefix then
+                    string_after sw @@ String.length prefix
+                else
+                    strip_prefix sw prefixes
+    let prefix prefix sw =
+        String.concat "" [prefix; sw]
+end
+module Opt = struct
+    type t = {
+        effect: Effect.t;
+        switches: string list;
+        in_usage: bool;
+        help: string option
+    }
+    let lookup_sw_opt sw prefixes opts =
+        let sw = Switch.strip_prefix sw prefixes in
+        List.find_opt
+            (fun opt ->
+                List.exists
+                    (fun s ->
+                        s = sw)
+                    opt.switches)
+            opts
+    let lookup_sw sw prefixes opts =
+        match lookup_sw_opt sw prefixes opts with
+            None -> raise @@ Unknown_switch sw
+            | Some opt -> opt
+    let known sw prefixes opts =
+        match lookup_sw_opt sw prefixes opts with
+            None -> false
+            | _ -> true
+    let display prefix opt =
+        String.concat ""
+            ["["; prefix; List.hd opt.switches; "]"]
+    let display_list prefix opts =
+        String.concat " "
+        @@ List.flatten
             [
-                usage cmd;
-                Option.fold ~none:"" ~some:String.trim cmd.synopsis;
+                List.map (display prefix)
+                @@ List.filter
+                    (fun opt ->
+                        match opt.effect with
+                            Rest -> false
+                            | _ -> opt.in_usage)
+                    opts;
+                if List.exists
+                    (fun opt -> not opt.in_usage)
+                    opts then
+                        ["[OPTION ...]"]
+                else
+                    [];
+                List.map (display prefix)
+                @@ Option.fold
+                    ~none:[]
+                    ~some:(fun x -> [x])
+                @@ List.find_opt
+                    (fun opt ->
+                        match opt.effect with
+                            Rest -> opt.in_usage
+                            | _ -> false)
+                    opts
+            ]
+    let help prefix opts =
+        let switches_width =
+            List.fold_left
+                (fun w opt ->
+                    let s =
+                        String.concat ", "
+                        @@ List.map (Switch.prefix prefix)
+                            opt.switches in
+                    max (String.length s) w)
+                0
+                opts in
+        if List.exists
+            (fun opt ->
+                Option.is_some opt.help || not opt.in_usage)
+            opts then
                 String.concat "\n"
+                @@ List.flatten
                     [
-                        "arguments:";
-                        String.concat "\n"
-                        @@ List.map
-                            (fun arg ->
-                                String.concat ""
-                                    [
-                                        "  ";
-                                        Printf.sprintf "%-*s"
-                                            arguments_width
-                                            arg.metavar;
-                                        "  ";
-                                        Option.fold
-                                            ~none:""
-                                            ~some:String.trim
-                                            arg.help
-                                    ])
-                            cmd.args
-                    ];
-                String.concat "\n"
-                    [
-                        "options:";
-                        String.concat "\n"
-                        @@ List.map
+                        ["options:"];
+                        List.map
                             (fun opt ->
                                 String.concat ""
                                     [
@@ -152,17 +138,105 @@ let help cmd =
                                         Printf.sprintf "%-*s"
                                             switches_width
                                         @@ String.concat ", "
-                                        @@ List.map prefix_switch
+                                        @@ List.map (Switch.prefix prefix)
                                         opt.switches;
                                         "  ";
                                         Option.fold
-                                            ~none:""
+                                           ~none:"(undocumented)"
                                             ~some:String.trim
                                             opt.help
                                     ])
-                            cmd.opts
-                    ];
-                Option.fold ~none:"" ~some:String.trim cmd.epilogue
+                            opts
+                    ]
+        else
+            ""
+
+end
+module Arg = struct
+    type t = {
+        name: string;
+        store: Store.t;
+        help: string option
+    }
+    let display arg =
+        let name = arg.name in
+        match arg.store with
+            One _ | One_opt _ -> name
+            | Maybe _ | Maybe_opt _ ->
+                String.concat "" ["["; name; "]"]
+            | Some _ ->
+                String.concat "" [name; " ["; name; " ...]"]
+            | Any _ ->
+                String.concat "" ["["; name; " ...]"]
+    let display_list args =
+        String.concat " "
+        @@ List.map display
+            args
+    let help args =
+        let arguments_width =
+            List.fold_left
+                (fun w arg ->
+                    max (String.length arg.name) w)
+                0
+                args in
+        if List.exists
+            (fun arg ->
+                Option.is_some arg.help)
+            args then
+                String.concat "\n"
+                @@ List.flatten
+                    [
+                        ["arguments:"];
+                        List.map
+                            (fun arg ->
+                                String.concat ""
+                                    [
+                                        "  ";
+                                        Printf.sprintf "%-*s"
+                                            arguments_width
+                                            arg.name;
+                                        "  ";
+                                        String.trim
+                                        @@ Option.get arg.help
+                                    ])
+                        @@ List.filter
+                            (fun arg ->
+                                Option.is_some arg.help)
+                            args
+                    ]
+        else
+            ""
+
+end
+type cmd = {
+    name: string;
+    switch_prefixes: string list;
+    opts: Opt.t list;
+    args: Arg.t list;
+    synopsis: string option;
+    help: string option
+}
+let usage cmd =
+    Printf.sprintf
+        "usage: %s"
+        @@ String.concat " "
+        @@ List.cons cmd.name
+        @@ List.filter
+            (fun s -> String.trim s != "")
+            [
+                Opt.display_list (List.hd cmd.switch_prefixes) cmd.opts;
+                Arg.display_list cmd.args
+            ]
+let help cmd =
+    String.concat "\n\n"
+        @@ List.filter
+            (fun s -> String.trim s != "")
+            [
+                usage cmd;
+                Option.fold ~none:"" ~some:String.trim cmd.synopsis;
+                Arg.help cmd.args;
+                Opt.help (List.hd cmd.switch_prefixes) cmd.opts;
+                Option.fold ~none:"" ~some:String.trim cmd.help
             ]
 let rec apply_cmd argv ?(args=[]) cmd =
     match argv with
@@ -171,7 +245,7 @@ let rec apply_cmd argv ?(args=[]) cmd =
                 let n_args = List.length args in
                 let min_n_args =
                     List.fold_left
-                        (fun n arg ->
+                        (fun n (arg: Arg.t) ->
                             max n @@
                             match arg.store with
                                 One _ | One_opt _ | Some _ -> 1
@@ -192,25 +266,29 @@ let rec apply_cmd argv ?(args=[]) cmd =
             apply_args args cmd.args
         | arg :: argv ->
             match arg with
-                | _ when is_switch arg && known_switch arg cmd.opts ->
-                    let opt = lookup_cmdopt arg cmd.opts in
-                    begin match opt.effect with
-                        Rest ->
-                            apply_cmd [] cmd ~args:(args @ argv)
-                        | Print_help ->
-                            raise @@ Print_help
-                        | Print_usage ->
-                            raise @@ Print_usage
-                       | Print_string s ->
-                            raise @@ Print s
-                        | Set_bool b ->
-                            b := true;
-                            apply_cmd argv cmd ~args:args
-                        | Inc_int i ->
-                            i := !i + 1;
-                            apply_cmd argv cmd ~args:args
-                    end
-                | _ when is_switch arg ->
+                | _ when
+                    Switch.is_switch arg cmd.switch_prefixes
+                    && Opt.known arg cmd.switch_prefixes cmd.opts ->
+                        let opt =
+                            Opt.lookup_sw
+                                arg cmd.switch_prefixes cmd.opts in
+                        begin match opt.effect with
+                            Rest ->
+                                apply_cmd [] cmd ~args:(args @ argv)
+                            | Print_help ->
+                                raise @@ Print_help
+                            | Print_usage ->
+                                raise @@ Print_usage
+                            | Print_string s ->
+                                raise @@ Print s
+                            | Set_bool b ->
+                                b := true;
+                                apply_cmd argv cmd ~args:args
+                            | Inc_int i ->
+                                i := !i + 1;
+                                apply_cmd argv cmd ~args:args
+                        end
+                | _ when Switch.is_switch arg cmd.switch_prefixes ->
                     raise @@ Unknown_switch arg
                 | _ ->
                     apply_cmd argv cmd ~args:(args @ [arg])
